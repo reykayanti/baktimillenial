4e6a77ea4d815cfded882c89bb6ecd3732e04431
amellptrr <amellptrr@Gmail.com>
2021-12-02 14:44:02 +0700

    ci4

--- Bakti Millenial/application/config/config.php
+++ Bakti Millenial/application/config/config.php
@@ -23,8 +23,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
 
+$root = "http://" .$_SERVER['HTTP_HOST'];
+$root .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
+$config['base_url'] = $root;
 /*
 |--------------------------------------------------------------------------
 | Index File
@@ -182,6 +184,7 @@ $config['permitted_uri_chars'] = 'a-z 0-9~%.:_\-';
 | use segment based URLs.
 |
 */
+
 $config['enable_query_strings'] = FALSE;
 $config['controller_trigger'] = 'c';
 $config['function_trigger'] = 'm';
