03fb1879394650011ab4825aee04b176108804f1
Retno Ekayanti <80891391+reykayanti@users.noreply.github.com>
2022-01-03 21:11:19 +0700

    foto

---
+++ CIbaru/public/frontend2/daftar.php
@@ -0,0 +1,152 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style/galery.css">
+    <!-- Bootstrap CSS -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+    <title>Bakti Millenial</title>
+    <?php
+    include('nav-bar.php');
+    ?>
+
+    <div class="container" class="mx-auto" style="width: 700px;">
+        <span class="border-0">
+            <center>
+                <h3>Form Pendaftaran</h3>
+            </center>
+            <form>
+                <div class=" form-group">
+                    <label for="exampleInputEmail1">Email</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                        placeholder="Masukkan email">
+                </div>
+                <div class="form-group">
+                    <label for="nama">Nama Lengkap</label>
+                    <input type="text" class="form-control" id="nama" aria-describedby="nama"
+                        placeholder="Masukkan nama">
+                </div>
+                <div class="form-group">
+                    <label for="nomor">Nomor WA</label>
+                    <input type="text" class="form-control" id="nomor" aria-describedby="nomor"
+                        placeholder="Masukkan nomor">
+                </div>
+                <div class="form-group">
+                    <label for="tgl_lahir">Tanggal Lahir</label>
+                    <input type="date" class="form-control" id="tgl_lahir" aria-describedby="tgl_lahir"
+                        placeholder="Masukkan tgl_lahir">
+                </div>
+                <div class="form-group">
+                    <label for="akunIG">Akun IG</label>
+                    <input type="text" class="form-control" id="akunIG" aria-describedby="akunIG"
+                        placeholder="Masukkan akunIG">
+                </div>
+                <div class="form-group">
+                    <label for="alamat">Alamat Domisili</label>
+                    <input type="text" class="form-control" id="alamat" aria-describedby="alamat"
+                        placeholder="Masukkan alamat">
+                </div>
+                <div class="form-group">
+                    <label for="instansi">Asal instansi (sekolah/kampus/perusahaan/lulusan/SMA)</label>
+                    <input type="text" class="form-control" id="instansi" aria-describedby="instansi"
+                        placeholder="Masukkan instansi">
+                </div>
+                <div class="form-group">
+                    <label for="jurusan">Jurusan</label>
+                    <input type="text" class="form-control" id="jurusan" aria-describedby="jurusan"
+                        placeholder="Masukkan jurusan">
+                </div>
+                <div class="form-group">
+                    <label for="akunIG">Tanggal Lahir</label>
+                    <input type="text" class="form-control" id="akunIG" aria-describedby="akunIG"
+                        placeholder="Masukkan akunIG">
+                </div>
+                <div class="form-group">
+                    <label for="pertama">Darimana kamu mengetahui informasi tentang Millenial Mengabdi Lombok
+                        ini?</label>
+                    <input type="text" class="form-control" id="pertama" aria-describedby="pertama"
+                        placeholder="Masukkan alasan">
+                </div>
+                <div class="form-group">
+                    <label for="kedua">Mengapa kamu ingin bergabung menjadi peserta millenial mengabdi 1.0 Lombok
+                        ini?</label>
+                    <input type="text" class="form-control" id="kedua" aria-describedby="kedua"
+                        placeholder="Masukkan alasan">
+                </div>
+                <div class="form-group">
+                    <label for="ketiga">Jika menjadi peserta terpilih, bidang apa yang ingin kamu pilih? (Pendidikan /
+                        Kesehatan/ Lingkungan / Ekonomi / Kreatif / Dokumentasi)</label>
+                    <input type="text" class="form-control" id="ketiga" aria-describedby="ketiga"
+                        placeholder="Masukkan alasan">
+                </div>
+                <div class="form-group">
+                    <label for="keempat">Apa alasan kamu memilih biding tersebut?</label>
+                    <input type="text" class="form-control" id="keempat" aria-describedby="keempat"
+                        placeholder="Masukkan alasan">
+                </div>
+                <div class="form-group">
+                    <label for="kelima">Inovasi program apa yang akan kamu berikan jika terpilih menjadi peserta di
+                        Millenial
+                        Mengabdi ini?</label>
+                    <input type="text" class="form-control" id="kelima" aria-describedby="kelima"
+                        placeholder="Masukkan alasan">
+                </div>
+                <div class="form-group">
+                    <label for="keenam">Apakah kamu memiliki relasi dengan komunitas, perusahaan, atau lain sebagainya?
+                        Menurutmmu pihak mana saja yang bisa kamu ajak berkolaborasi / kerjasama dalam kegiatan
+                        Millenial
+                        Mengabdi 1.0 ini></label>
+                    <input type="text" class="form-control" id="keenam" aria-describedby="keenam"
+                        placeholder="Masukkan alasan">
+                </div>
+                <form>
+                    <div class="form-group">
+                        <label for="exampleFormControlFile1">Upload CV</label>
+                        <input type="file" class="form-control-file" id="exampleFormControlFile1">
+                    </div>
+                </form>
+                <div class="form-group form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </span>
+    </div>
+    <script>
+    var galery = document.getElementById("galery");
+    galery.classList.add('active');
+    galery.setAttribute("aria-current", "page");
+
+    var home = document.getElementById("home");
+    home.classList.remove('active');
+    home.setAttribute("aria-current", "none");
+
+    var program = document.getElementById("program");
+    program.classList.remove('active');
+    program.setAttribute("aria-current", "none");
+
+    var daftar = document.getElementById("daftar");
+    daftar.classList.remove('active');
+    daftar.setAttribute("aria-current", "none");
+    </script>
+
+    <!-- Javascript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
+</head>
+
+</html>
---
+++ CIbaru/public/frontend2/nav-bar.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style/nav-bar.css">
+
+    <title>Bakti Millenial</title>
+
+<body>
+    <!-- Navbar content -->
+    <nav class="navbar navbar-expand-lg navbar-light shadow p-3 w-auto"
+        style="background-color: #96bb7c; border-radius: 0px 0px 40px 40px; width: 100px; z-index: 2;">
+        <button class="navbar-toggler" id="nav-icon" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbar-toogler" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <a class="navbar-brand"></a>
+        <img src="images/logobm.svg">
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0 px-3 fs-5">
+                <li>
+                    <a id="home" class="nav-link active" id="nav-btn" aria-current="page" href="home.php">Home</a>
+                </li>
+                <li>
+                    <a id="galery" class="nav-link" href="galery.php">Galery</a>
+                </li>
+                <li>
+                    <a id="program" class="nav-link" href="program.php">Program</a>
+                </li>
+                <li>
+                    <button type="button" class="btn btn-light h-1 mt-1 ms-auto ms-lg-5" href="#"
+                        style="background: #d6efc7; border-radius: 40px; width: 122px;">Sign</button>
+                </li>
+            </ul>
+            <form class="d-flex">
+                <input class="form-control" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+        </div>
+        </div>
+    </nav>
+    <!-- end navbar contect -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
+    </head>
+</body>
+
+</html>
