75cae03aadd66d693ab5efe88bf1da1b7c8223b2
Ulfatun21 <ulfatun945@gmail.com>
2022-01-13 19:55:07 +0700

    revisi frontend

--- CIbaru/app/Views/frontend/v_program.php
+++ CIbaru/app/Views/frontend/v_program.php
@@ -1,86 +1,102 @@
 <?= $this->extend('template/v_template_front'); ?>
 <?= $this->section('content'); ?>
-        <div class="container" style="margin-top: 100px;">
-            <div class="row">
-                <div class="col-lg-6 col-md-12">
-                    <center>
-                        <img src="frontend2/images/sub-cont.svg" style="width: 380px;" height="400px;">
-                    </center>
-                </div>
-                <div class="col-lg-6 col-md-12 mt-md-5">
-                    <h2>Milenial Mengabdi #4 Lombok</h2>
-                    <p class="card-text"><small class="text-muted">Updated 15 Desember 2021</small></p>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                        deserunt mollit anim id est laborum.
-                    </p>
-                    <a href="Cdetailprogram" class="btn btn-primary" img src="frontend2/images/READ.svg" style="background: #184D47; font-size:x-small;">Read More</a>
-                </div>
 
-                <div class="col-lg-6 col-md-12 mt-md-5 mt-sm-5">
-                    <h2>Milenial Mengabdi #4 Lombok</h2>
-                    <p class="card-text"><small class="text-muted">Updated 15 Desember 2021</small></p>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                        deserunt mollit anim id est laborum.
-                    </p>
-                    <a href="Cdetailprogram" class="btn btn-primary" img src="images/READ.svg" style="background: #184D47; font-size:x-small;">Read More</a>
-                </div>
-                <div class="col-lg-6 col-md-12">
-                    <center>
-                        <img src="frontend2/images/sub-cont.svg" style="width: 430px;" height="450px;">
-                    </center>
-                </div>
+<head>
+    <link rel="stylesheet" href="frontend2/style/program.css">
+</head>
 
-                <div class="col-lg-6 col-md-12">
-                    <center>
-                        <img src="frontend2/images/sub-cont.svg" style="width: 430px;" height="450px;">
-                    </center>
-                </div>
-                <div class="col-lg-6 col-md-12 mt-md-5">
-                    <h2>Milenial Mengabdi #4 Lombok</h2>
-                    <p class="card-text"><small class="text-muted">Updated 15 Desember 2021</small></p>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                        deserunt mollit anim id est laborum.
-                    </p>
-                    <a href="Cdetailprogram" class="btn btn-primary" img src="frontend2/images/READ.svg" style="background: #184D47; font-size:x-small;">Read More</a>
-                </div>
+<div class="container py-5">
+    <h1 class="title text-center">MILENIAL MENGABDI #1</h1>
+    <h1 class="title text-center">LOMBOK</h1>
 
-                <div class="col-lg-6 col-md-12 mt-md-5 mt-sm-5">
-                    <h2>Milenial Mengabdi #4 Lombok</h2>
-                    <p class="card-text"><small class="text-muted">Updated 15 Desember 2021</small></p>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                        deserunt mollit anim id est laborum.
-                    </p>
-                    <a href="Cdetailprogram" class="btn btn-primary" img src="frontend2/images/READ.svg" style="background: #184D47; font-size:x-small;">Read More</a>
-                </div>
-                <div class="col-lg-6 col-md-12">
-                    <center>
-                        <img src="frontend2/images/sub-cont.svg" style="width: 430px;" height="450px;">
-                    </center>
-                </div>
+    <div class="row">
+        <div class="posisi-section">
+            <div class="col-lg-6 col-md-12">
+                <center>
+                    <img src="frontend2/images/sub-cont.svg" style="width: 380px;" height="400px;">
+                </center>
+            </div>
+            <div class="col-lg-6 col-md-12 mt-md-5 text-justify">
+                <h2>Bidang Lingkungan</h2>
+                <p class="card-text"><small class="text-muted">Updated 15 Desember 2021</small></p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.
+                </p>
+                <a href="Cdetailprogram" class="btn btn-primary" img src="frontend2/images/READ.svg" style="background: #184D47; font-size:x-small;">Read More</a>
             </div>
         </div>
+
+        <div class="posisi-section">
+            <div class="col-lg-6 col-md-12 mt-md-5 mt-sm-5 text-justify">
+                <h2>Bidang Pendidikan</h2>
+                <p class="card-text"><small class="text-muted">Updated 15 Desember 2021</small></p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.
+                </p>
+                <a href="Cdetailprogram" class="btn btn-primary" img src="images/READ.svg" style="background: #184D47; font-size:x-small;">Read More</a>
+            </div>
+            <div class="col-lg-6 col-md-12">
+                <center>
+                    <img src="frontend2/images/sub-cont.svg" style="width: 430px;" height="450px;">
+                </center>
+            </div>
+        </div>
+
+        <div class="posisi-section">
+            <div class="col-lg-6 col-md-12">
+                <center>
+                    <img src="frontend2/images/sub-cont.svg" style="width: 430px;" height="450px;">
+                </center>
+            </div>
+            <div class="col-lg-6 col-md-12 mt-md-5 text-justify">
+                <h2>Bidang Kesehatan</h2>
+                <p class="card-text"><small class="text-muted">Updated 15 Desember 2021</small></p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.
+                </p>
+                <a href="Cdetailprogram" class="btn btn-primary" img src="frontend2/images/READ.svg" style="background: #184D47; font-size:x-small;">Read More</a>
+            </div>
+        </div>
+
+        <div class="posisi-section">
+            <div class="col-lg-6 col-md-12 mt-md-5 mt-sm-5 text-justify">
+                <h2>Bidang Ekonomi Kreatif</h2>
+                <p class="card-text"><small class="text-muted">Updated 15 Desember 2021</small></p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.
+                </p>
+                <a href="Cdetailprogram" class="btn btn-primary" img src="frontend2/images/READ.svg" style="background: #184D47; font-size:x-small;">Read More</a>
+            </div>
+            <div class="col-lg-6 col-md-12">
+                <center>
+                    <img src="frontend2/images/sub-cont.svg" style="width: 430px;" height="450px;">
+                </center>
+            </div>
+        </div>
+    </div>
+</div>
 <?= $this->endSection(); ?>
