03cedf7b7d7c972f7c0b18a0bc979c28fd93aeed
Retno Ekayanti <80891391+reykayanti@users.noreply.github.com>
2022-01-13 21:19:12 +0700

    tambah buat sign in

---
+++ CIbaru/app/Views/frontend/v_login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="/frontend2/style/login.css" />
+    <title>Login</title>
+  </head>
+  <body>
+    <div class="cont-ek">
+      <div class="kr subcont"></div>
+      <div class="kn subcont">
+        <div class="isi">
+          <h1><b>SIGN IN</b></h1>
+          <form action="">
+            <input type="text" name="username" id="username" class="" value="Username" required /> <br />
+            <input type="text" name="password" id="password" class="ps" value="password" required />
+            <a href="#"><div class="submit">Login</div></a>
+          </form>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
