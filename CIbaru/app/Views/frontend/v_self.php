5b83ab9780b1ef8769899d91a0009711786a7923
Ulfatun21 <ulfatun945@gmail.com>
2022-01-15 22:22:44 +0700

    update konten2 fix

--- CIbaru/app/Views/frontend/v_self.php
+++ CIbaru/app/Views/frontend/v_self.php
@@ -1,8 +1,13 @@
 <?= $this->extend('template/v_template_front'); ?>
 <?= $this->section('content'); ?>
 
+<!-- fonts -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Acme&family=Viga&display=swap" rel="stylesheet">
+
 <head>
-    <link rel="stylesheet" href="frontend2/style/detail-program.css">
+    <link rel="stylesheet" href="frontend2/style/jalurself.css">
 </head>
 
 <div class="container text-justify py-5">
@@ -53,12 +58,18 @@
     </table>
 </div>
 
-<div class="contaner-fluid text-center py-5" style="background-color: #FAD586;">
-    <h1>Skema Seleksi</h1>
-    <p>Peserta membayar biaya pendaftaran sebesar Rp25.000 dan mengisi kelengkapan berkas</p>
-    <p>Tanpa Seleksi</p>
-    <p>Pengumuman melalui email dan mendapat konfirmasi melalui WhatsApp dari official akun Milenial Mengabdi</p>
-    <p>Pembayaran jalur Self Funded dapat dilakukan secara bertahap atau langsung lunas</p>
+<div class="container-fluid text-center py-5" style="background-color: #FAD586;">
+    <div class="row">
+        <div class="col-4 pt-3 judul">
+            <h1>Skema</h1>
+            <h1 class="judul2">Seleksi</h1>
+        </div>
+        <div class="col-7 text-justify">
+            <p>Peserta membayar biaya pendaftaran sebesar Rp25.000 dan mengisi kelengkapan berkas</p>
+            <p>Tanpa Seleksi</p>
+            <p>Pengumuman melalui email dan mendapat konfirmasi melalui WhatsApp dari official akun Milenial Mengabdi</p>
+            <p>Pembayaran jalur Self Funded dapat dilakukan secara bertahap atau langsung lunas</p>
+        </div>
+    </div>
 </div>
-
 <?= $this->endSection(); ?>
--- CIbaru/app/Views/frontend/v_special.php
+++ CIbaru/app/Views/frontend/v_special.php
@@ -2,8 +2,13 @@
 <?= $this->section('content'); ?>
 
 <head>
-    <link rel="stylesheet" href="frontend2/style/detail-program.css">
+    <link rel="stylesheet" href="frontend2/style/jalurself.css">
 </head>
+
+<!-- fonts -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Acme&family=Viga&display=swap" rel="stylesheet">
 
 <div class="container text-justify py-5">
     <h1 class="title">Jalur Pendaftaran</h1>
@@ -63,12 +68,19 @@
     </table>
 </div>
 
-<div class="contaner-fluid text-center py-5" style="background-color: #FAD586;">
-    <h1>Skema Seleksi</h1>
-    <p>Jalur rekomendasi dan prestasi, akan diberikan potongan biaya oleh Yayasan Karya Anak Milenial Indonesia</p>
-    <p>Peserta membayar biaya pendaftaran sebesar Rp25.000 dan mengisi kelengkapan berkas</p>
-    <p>Peserta melaksanakan assessment test tulis</p>
-    <p>Pengumuman peserta lolos jalur Special Funded</p>
-    <p>Pembayaran jalur Special Funded dapat dilakukan secara bertahap atau langsung lunas</p>
+<div class="container-fluid text-center py-5" style="background-color: #FAD586;">
+    <div class="row">
+        <div class="col-4 pt-3 judul">
+            <h1>Skema</h1>
+            <h1 class="judul2">Seleksi</h1>
+        </div>
+        <div class="col-7 text-justify">
+            <p>Jalur rekomendasi dan prestasi, akan diberikan potongan biaya oleh Yayasan Karya Anak Milenial Indonesia</p>
+            <p>Peserta membayar biaya pendaftaran sebesar Rp25.000 dan mengisi kelengkapan berkas</p>
+            <p>Peserta melaksanakan assessment test tulis</p>
+            <p>Pengumuman peserta lolos jalur Special Funded</p>
+            <p>Pembayaran jalur Special Funded dapat dilakukan secara bertahap atau langsung lunas</p>
+        </div>
+    </div>
 </div>
 <?= $this->endSection(); ?>
