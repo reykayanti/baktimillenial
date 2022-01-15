f2b3a48c998a62d3a98e32c0ada0922f62a84df4
muhamadrizki28 <gramansite@gmail.com>
2022-01-15 21:22:45 +0700

    fix

--- CIbaru/app/Views/frontend/v_home.php
+++ CIbaru/app/Views/frontend/v_home.php
@@ -113,6 +113,7 @@
                     </div>
                     <div class="item">
                         <div class="pad15">
+<<<<<<< HEAD
                             <p class="lead">Lingkungan</p>
                             <img src="frontend2/images/satu.png" class="rounded-circle" width="70" height="70">
                             <p>Dalam bidang lingkungan, Lombok sebagai tempat tujuan destinasi wisata tentunya memiliki
@@ -158,7 +159,7 @@
                         <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
                     </svg>
                 </button>
-                <<<<<<< HEAD </div>
+                </div>
             </div>
 
             <div class="container-fluid pt-2 p-3 my-3 text-grey-700" style="background: #FAD586">
@@ -285,8 +286,6 @@
                 </div>
             </div>
         </div>
-
-
 
         <script>
             $(document).ready(function() {
