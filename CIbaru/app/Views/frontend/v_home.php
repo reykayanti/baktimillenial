2dc75d4772159d49996a57628543d1a732fae92b
Ulfatun21 <ulfatun945@gmail.com>
2022-01-15 19:52:05 +0700

    Revert "Revert "update konten2""
    This reverts commit 2cc580e225f7f3d921027101b10a903dee46c823.

--- CIbaru/app/Views/frontend/v_home.php
+++ CIbaru/app/Views/frontend/v_home.php
@@ -4,6 +4,7 @@
 <head>
     <link rel="stylesheet" href="frontend/style/home.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+
     <style>
         .MultiCarousel {
             float: left;
@@ -15,7 +16,8 @@
         }
 
         .MultiCarousel {
-            Height: 189px;
+            Height: 550px;
+
         }
 
         .MultiCarousel .MultiCarousel-inner {
@@ -29,9 +31,9 @@
 
         .MultiCarousel .MultiCarousel-inner .item>div {
             text-align: center;
-            padding: 10px;
-            margin: 10px;
-            background: #f1f1f1;
+            padding: 20px;
+            margin: 20px;
+            background: #E3F6D7;
             color: #666;
         }
 
@@ -54,6 +56,18 @@
         .MultiCarousel .rightLst.over {
             pointer-events: none;
             background: #ccc;
+        }
+
+        .lead {
+            font-family: bold;
+        }
+
+        .border {
+            text-align: center;
+            padding: 20px;
+            margin: 20px;
+            background: #E3F6D7;
+            color: #666;
         }
     </style>
 </head>
@@ -81,43 +95,60 @@
             </div>
         </div>
     </div>
-</div>
 
-<div class="container">
-    <h2>Projects</h2>
-    <div class="row">
-        <div class="MultiCarousel" data-items="1,3,5,6" data-slide="1" id="MultiCarousel" data-interval="1000">
-            <div class="MultiCarousel-inner">
-                <div class="item">
-                    <div class="pad15">
-                        <p class="lead">Multi Item Carousel</p>
-                        <p>₹ 1</p>
-                        <p>₹ 6000</p>
-                        <p>50% off</p>
+    <div class="container">
+        <center>
+            <h2>Projects</h2>
+        </center>
+        <div class="row">
+            <div class="MultiCarousel" data-items="1,3,5,6" data-slide="1" id="MultiCarousel" data-interval="1000">
+                <div class="MultiCarousel-inner">
+                    <div class="item">
+                        <div class="pad15">
+                            <p class="lead">Pendidikan</p>
+                            <img src="frontend2/images/satu.png" class="rounded-circle" width="70" height="70">
+                            <p>Di bidang pendidikan, kurangnya motivasi belajar dan kesadaran akan pentingnya pendidikan
+                                merupakan permasalahan yang sering di keluhkan masyarakat sedikitnya lulusan Sekolah
+                                Menengah Atas di Lombok yang melanjutkan ke perguruan tinggi, serta sarana prasarana
+                                yang kurang memadai dan hampir tidak ada koleksi buku panduan pendidik di perpustakaan
+                                sekolah. </p>
+                        </div>
                     </div>
-                </div>
-                <div class="item">
-                    <div class="pad15">
-                        <p class="lead">Multi Item Carousel</p>
-                        <p>₹ 1</p>
-                        <p>₹ 6000</p>
-                        <p>50% off</p>
+                    <div class="item">
+                        <div class="pad15">
+                            <p class="lead">Lingkungan</p>
+                            <img src="frontend2/images/satu.png" class="rounded-circle" width="70" height="70">
+                            <p>Dalam bidang lingkungan, Lombok sebagai tempat tujuan destinasi wisata tentunya memiliki
+                                permasalahan pada sampah, dimana kurang adanya kesadaran akan kebersihan lingkungan dan
+                                juga pengelolaan sampah secara berkelanjutan. Selain sampah, limbah kotoran ternak juga
+                                menjadi masalah yang belum terselesaikan karena belum adanya langkah dalam pengolahan
+                                limbah tersebut. </p>
+                        </div>
                     </div>
-                </div>
-                <div class="item">
-                    <div class="pad15">
-                        <p class="lead">Multi Item Carousel</p>
-                        <p>₹ 1</p>
-                        <p>₹ 6000</p>
-                        <p>50% off</p>
+                    <div class="item">
+                        <div class="pad15">
+                            <p class="lead">Kesehatan</p>
+                            <img src="frontend2/images/satu.png" class="rounded-circle" width="70" height="70">
+                            <p>Di bidang kesehatan, minimnya fasilitas yang ada menyebabkan masyarakat mengalami
+                                kesulitan untuk menjangkau puskesmas yang cukup jauh dari lokasi mereka. Minimnya
+                                pemahaman masyarakat mengenai penerapan pola hidup bersih dan sehat, serta pengetahuan
+                                tentang pendidikan menjadi keresahan tersendiri. Kesehatan psikologis juga menjadi
+                                permasalahan terutama pada pernikahan anak usia dini yang banyak terjadi di Lombok.
+                            <p>
+                        </div>
                     </div>
-                </div>
-                <div class="item">
-                    <div class="pad15">
-                        <p class="lead">Multi Item Carousel</p>
-                        <p>₹ 1</p>
-                        <p>₹ 6000</p>
-                        <p>50% off</p>
+                    <div class="item">
+                        <div class="pad15">
+                            <p class="lead">Ekonomi Kreatif</p>
+                            <img src="frontend2/images/satu.png" class="rounded-circle" width="70" height="70">
+                            <p>Sebagai tujuan destinasi wisata tentunya masyarakat harus memiliki skill yang inovatif
+                                dan juga kreatif untuk dapat membuat karya sehingga menjadi nilai jual kepada para
+                                wisatawan dan juga masyarakat yang berkunjung. Perlu adanya pelatihan atau pembinaan
+                                dalam meningkatkannya sehingga bisa muncul ide kreatif dan juga mampu menjadi manusia
+                                yang unggul. Proses pemasaran produk, terlalu berfokus pada satu komoditas, kurang bisa
+                                memanfaatkan media sosial/digitalisasi dalam memasarkan produk, kurangnya kreativitas
+                                dalam mengolah </p>
+                        </div>
                     </div>
                 </div>
                 <!-- <div class="item">
@@ -130,42 +161,48 @@
                 </div>-->
             </div>
             <button class="btn btn-primary leftLst">
-                << /button>
-                    <button class="btn btn-primary rightLst">></button>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-circle" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z" />
+                </svg>
+            </button>
+            <button class="btn btn-primary rightLst">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
+                </svg>
+            </button>
         </div>
+        <button class="btn btn-primary leftLst">
+            << /button>
+                <button class="btn btn-primary rightLst">></button>
     </div>
 </div>
 
-
-<div class="container-fluid pt-2 p-3 my-3 text-white" style="background: #FAD586">
+<div class="container-fluid pt-2 p-3 my-3 text-grey-700" style="background: #FAD586">
     <div class="row">
-        <!-- <div class="d-flex justify-content-center align-items-center flex-column col-6"> -->
-        <div class="col-7 text-justify py-5 ms-5 m-auto">
-            <h1 class="text-end">BAKTI MILLENIAL</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-                incididunt ut
-                labore
-                et
-                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut
-                aliquip
-                ex
-                ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum
-                dolore
-                eu
-                fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-                officia
-                deserunt
-                mollit
-                anim id est laborum.</p>
-            <a href="#" class="btn btn-success">Read more</a>
+        <div class="d-flex justify-content-center align-items-center flex-column col-7">
+            <h1>BAKTI MILLENIAL</h1>
+            <p>Bakti Milenial merupakan sebuah program dibawah naungan Indonesia Milenial Connect (IMC).
+                Indonesia Milenial Connect merupakan wadah pengembangan diri bagi pemuda/i Indonesia baik yang
+                berada dalam negeri maupun luar negeri, berfokus kepada tiga bidang yang menjadi faktor
+                kesejahteraan pemberdayaan masyarakat yaitu, pendidikan, sosial, dan ekonomi.
+                Pada Juli 2020, IMC resmi legal membentuk sebuah yayasan bernama Karya Anak Milenial Indonesia
+                dan Bapak Sandiaga Uno sebagai Dewan Penasehat.
+                Bakti Milenial hadir sebagai kontribusi nyata bagi bangsa Indonesia dalam bidang pemberdayaan
+                masyarakat khususnya dlam mengembangkan kesejahteraan dan kemajuan bangsa. Adapun tema yang
+                diusung yaitu “Optimizing the Use of Local Potential to Organize Sustainable Community
+                Development”.
+                Lokasi pengabdian masyarakat ini berada di pulau Bintan, Provinsi Kepulauan Riau. </p>
+            <a href="#" class="btn rounded-pill" style="background: #184D47">About IMC</a>
         </div>
-        <div class="col-3 m-auto">
-            <img src="frontend2/images/BINTAN.svg" width="400" height="400">
+        <div class="col-2">
+            <img src="frontend2/images/lombok.png" width="400" height="400">
         </div>
     </div>
+</div>
+<div class="col-3 m-auto">
+    <img src="frontend2/images/BINTAN.svg" width="400" height="400">
+</div>
+</div>
 </div>
 
 <div class="row pt-2">
@@ -194,6 +231,64 @@
             </div>
             <p class="card-text">Keseluruhan biaya ditanggung pribadi.</p>
         </div>
+        <div class="container-fluid pt-2 m-9 my-3 text-grey-700" style="background: #FAD586">
+            <div class="row">
+                <div class="d-flex justify-content-center align-items-center flex-column col-7">
+                    <h1>Lokasi</h1>
+                    <p>Bakti Milenial merupakan sebuah program dibawah naungan Indonesia Milenial Connect (IMC).
+                        Indonesia Milenial Connect merupakan wadah pengembangan diri bagi pemuda/i Indonesia baik yang
+                        berada dalam negeri maupun luar negeri, berfokus kepada tiga bidang yang menjadi faktor
+                        kesejahteraan pemberdayaan masyarakat yaitu, pendidikan, sosial, dan ekonomi.
+                        Pada Juli 2020, IMC resmi legal membentuk sebuah yayasan bernama Karya Anak Milenial Indonesia
+                        dan Bapak Sandiaga Uno sebagai Dewan Penasehat.
+                        Bakti Milenial hadir sebagai kontribusi nyata bagi bangsa Indonesia dalam bidang pemberdayaan
+                        masyarakat khususnya dlam mengembangkan kesejahteraan dan kemajuan bangsa. Adapun tema yang
+                        diusung yaitu “Optimizing the Use of Local Potential to Organize Sustainable Community
+                        Development”.
+                        Lokasi pengabdian masyarakat ini berada di pulau Bintan, Provinsi Kepulauan Riau. </p>
+                </div>
+                <div class="col-2">
+                    <img src="frontend2/images/lokasi.png" width="400" height="400">
+                </div>
+            </div>
+        </div>
+
+
+        <div class="container">
+            <center>
+                <h2>Persyaratan & Ketentuan</h2>
+            </center>
+            <div class="row">
+                <div class="d-flex justify-content-center align-items-center">
+                    <div class="border col-lg-6 col-md-12 mt-md-5">
+                        <p>Terbuka bagi pelajar, mahasiswa,atau masyarakat umum Indonesia dengan batasan usia
+                            15-35 tahun.</p>
+                        <p>Bersedia mengikuti kegiatan dari awal (pembekalan sampai akhir kegiatan</p>
+                        <p>Bersungguh-sungguh dan memiliki niat yang ikhlas dalam kegiatan pengabdian
+                            masyarakat.</p>
+                        <p>Memiliki komitmen diri terhadap kegiatan pengabdian.</p>
+
+                    </div>
+                    <div class="border col-lg-6 col-md-12">
+                        <p>Berjiwa sosial dan mampu bekerja dalam tim.</p>
+                        <p>Aktif, kreatif, inovatif, baik dalam pra kegitan, proses kegiatan, maupun pasca
+                            kegiatan.</p>
+                        <p>Mengikuti seluruh alur tata cara pendaftaran</p>
+                        <p>Mematuhi seluruh aturan selama pengabdian</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- <div class="container-fluid p-5 text-center text-white" style="background: #FAD586">
+            <p>Ikuti kami di sosial media untuk mendapatkan informasi mengenai event, volunteer, bakti
+                millenial, dan
+                perkembangan Bakti Millenial yang lainnya.</p>
+            <div class="flex">
+                <img src="frontend2/images/satu.png" class="rounded-circle" width="70" height="70">
+                <img src="frontend2/images/satu.png" class="rounded-circle" width="70" height="70">
+                <img src="frontend2/images/satu.png" class="rounded-circle" width="70" height="70">
+            </div>
+        </div> -->
     </div>
 </div>
 
@@ -307,7 +402,8 @@
                     $(el + ' ' + rightBtn).addClass("over");
                 }
             }
-            $(el + ' ' + itemsDiv).css('transform', 'translateX(' + -translateXval + 'px)');
+            $(el + ' ' + itemsDiv).css('transform', 'translateX(' + -translateXval +
+                'px)');
         }
 
         //It is used to get some elements from btn
