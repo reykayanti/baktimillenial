c833afaaf5e245481f78eab98e2e8427ad175561
muhamadrizki28 <gramansite@gmail.com>
2022-01-18 15:46:22 +0700

    Fix Mobile Responsive For Menu Navbar

--- CIbaru/app/Views/template/v_navbar.php
+++ CIbaru/app/Views/template/v_navbar.php
@@ -6,6 +6,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!-- FA 6
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" /> -->
+
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <!-- Font Awesome -->
@@ -26,13 +29,18 @@
     <!-- Navbar content -->
     <nav class="navbar fixed-top navbar-expand-lg navbar-light shadow p-3 w-auto navigasi-container" style="background-color: #96bb7c; border-radius: 0px 0px 40px 40px; width: 100px; z-index: 9;height:65px;filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.15));">
         <div class="container-fluid">
-            <button class="navbar-toggler" id="nav-icon" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+            <!-- <button class="navbar-toggler" id="nav-icon" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> -->
+                <!-- <span class="navbar-toggler-icon"></span> -->
+                <div class="mobile-bar">
+                    <span class="mobile-bar1 mobile-bar-all"></span>
+                    <span class="mobile-bar2 mobile-bar-all"></span>
+                    <span class="mobile-bar3 mobile-bar-all"></span>
+                </div>
+            <!-- </button> -->
             <a class="navbar-brand ms-5" href="<?= base_url('/'); ?>">
                 <img src="frontend2/images/logomm.svg" style="width: 45px; height: 45px;">
             </a>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="menu-nav collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0 px-3 fs-5">
                     <li class="me-5">
                         <!-- <a id="home" class="nav-link active" id="nav-btn" aria-current="page" href="Chome">Home</a> -->
@@ -78,9 +86,19 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 
     <script>
-        $(document).ready(function() {
-            $('.navbar-toggler').click(function() {
-                $('#navbarSupportedContent').toggleClass('active');
+        // $(document).ready(function() {
+        //     $('.navbar-toggler').click(function() {
+        //         $('#navbarSupportedContent').toggleClass('active');
+        //     });
+        // });
+
+        $(document).ready(function () {
+            $('.mobile-bar').click(function () {
+            $('#navbarSupportedContent').toggleClass('active');
+            $('.mobile-bar1').toggleClass('active');
+            $('.mobile-bar2').toggleClass('active');
+            $('.mobile-bar3').toggleClass('active');
+            // $('.a').toggleClass('active');
             });
         });
 
