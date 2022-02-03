c7758a7ff647ef697f64407f0ef6d42f185d59ad
unknown <denisearuan19@gmail.com>
2022-02-03 09:22:35 +0700

    home6

--- CIbaru/app/Views/frontend/v_home.php
+++ CIbaru/app/Views/frontend/v_home.php
@@ -3,74 +3,75 @@
 
 <head>
     <link rel="stylesheet" href="frontend/style/home.css">
+    <link rel="stylesheet" href="frontend/style/responsive.css">
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Acme&family=Viga&display=swap" rel="stylesheet">
     <style>
-    .MultiCarousel {
-        float: left;
-        overflow: hidden;
-        padding: 15px;
-        width: 100%;
-        position: relative;
-        margin: auto;
-    }
+        .MultiCarousel {
+            float: left;
+            overflow: hidden;
+            padding: 15px;
+            width: 100%;
+            position: relative;
+            margin: auto;
+        }
 
-    .MultiCarousel {
-        Height: 250px;
+        .MultiCarousel {
+            Height: 250px;
 
-    }
+        }
 
-    .MultiCarousel .MultiCarousel-inner {
-        transition: 1s ease all;
-        float: left;
-    }
+        .MultiCarousel .MultiCarousel-inner {
+            transition: 1s ease all;
+            float: left;
+        }
 
-    .MultiCarousel .MultiCarousel-inner .item {
-        float: left;
-    }
+        .MultiCarousel .MultiCarousel-inner .item {
+            float: left;
+        }
 
-    .MultiCarousel .MultiCarousel-inner .item>div {
-        text-align: center;
-        padding: 20px;
-        margin: 20px;
-        background: #E3F6D7;
-        color: #666;
-    }
+        .MultiCarousel .MultiCarousel-inner .item>div {
+            text-align: center;
+            padding: 20px;
+            margin: 20px;
+            background: #E3F6D7;
+            color: #666;
+        }
 
-    .MultiCarousel .leftLst,
-    .MultiCarousel .rightLst {
-        position: absolute;
-        border-radius: 50%;
-        top: calc(50% - 20px);
-    }
+        .MultiCarousel .leftLst,
+        .MultiCarousel .rightLst {
+            position: absolute;
+            border-radius: 50%;
+            top: calc(50% - 20px);
+        }
 
-    .MultiCarousel .leftLst {
-        left: 0;
-    }
+        .MultiCarousel .leftLst {
+            left: 0;
+        }
 
-    .MultiCarousel .rightLst {
-        right: 0;
-    }
+        .MultiCarousel .rightLst {
+            right: 0;
+        }
 
-    .MultiCarousel .leftLst.over,
-    .MultiCarousel .rightLst.over {
-        pointer-events: none;
-        background: #ccc;
-    }
+        .MultiCarousel .leftLst.over,
+        .MultiCarousel .rightLst.over {
+            pointer-events: none;
+            background: #ccc;
+        }
 
-    .lead {
-        font-family: bold;
-    }
+        .lead {
+            font-family: bold;
+        }
 
-    .border {
-        text-align: center;
-        padding: 20px;
-        margin: 20px;
-        background: #E3F6D7;
-        color: #666;
-    }
+        .border {
+            text-align: center;
+            padding: 20px;
+            margin: 20px;
+            background: #E3F6D7;
+            color: #666;
+        }
     </style>
 </head>
 
@@ -100,8 +101,7 @@
             </center>
         </div>
         <div class="row">
-            <div class="MultiCarousel" data-items="1,3,5,6" data-slide="1" id="MultiCarousel" data-interval="1000"
-                style="height: auto;">
+            <div class="MultiCarousel" data-items="1,3,5,6" data-slide="1" id="MultiCarousel" data-interval="1000" style="height: auto;">
                 <div class="MultiCarousel-inner" style="height: auto;">
                     <div class="item">
                         <div class="pad15">
@@ -210,9 +210,9 @@
 <div class="row pt-5 pb-5">
     <div class="col-6 pt-0 justify-content-center align-items-center">
         <h1>Jalur Pendaftaran</h1>
-        <img src="frontend2/images/ketentuan.png" class="pt-3" style="width:60%">
+        <img src="frontend2/images/ketentuan.png" class="jalur-img pt-3" style="width:60%">
     </div>
-    <div class="col-6 pt-0 py-5 p-lg-5 ">
+    <div class="jalur-grup col-6 pt-0 py-5 p-lg-5 ">
         <div class="flex jalur-container">
             <div class="jalur-box"></div>
             <div class="d-flex" id="">
@@ -239,7 +239,7 @@
 
 <!-- Lokasi -->
 <div class="container-fluid" style="background: #FAD586">
-    <div class="row" id="orange-satu">
+    <div class="row lokasi" id="orange-satu">
         <div class=" d-flex justify-content-center align-items-center flex-column col-lg-7 col-md-12">
             <h1>Lokasi</h1>
             <p class="text-justify" id="orange-dua">Bakti Milenial merupakan sebuah program dibawah naungan Indonesia
@@ -257,20 +257,19 @@
                 Development”.
                 Lokasi pengabdian masyarakat ini berada di pulau Bintan, Provinsi Kepulauan Riau. </p>
         </div>
-        <div class="col-2 py-5 ">
+        <div class="col-2 py-5">
             <img src="frontend2/images/lokasi.png" width="400" height="auto">
         </div>
     </div>
 </div>
-<div class="container">
+<div class="container syarat-ketentuan">
     <center>
         <h2>Persyaratan & Ketentuan</h2>
     </center>
     <div class="row">
         <div class="d-flex justify-content-center align-items-center">
-            <div class="border d-flex justify-content-center align-items-center flex-column col-lg-6 col-md-12"
-                id="syarat-box">
-                <div id="" class="">
+            <div class="border syarat-grup d-flex justify-content-center align-items-center flex-column col-lg-6 col-md-12" id="syarat-box">
+                <div id="syarat-text" class="">
                     <p>Terbuka bagi pelajar, mahasiswa,atau masyarakat umum Indonesia dengan batasan usia
                         15-35 tahun.</p>
                     <p>Bersedia mengikuti kegiatan dari awal (pembekalan sampai akhir kegiatan</p>
@@ -279,8 +278,7 @@
                     <p>Memiliki komitmen diri terhadap kegiatan pengabdian.</p>
                 </div>
             </div>
-            <div class="border d-flex justify-content-center align-items-center flex-column col-lg-6 col-md-12"
-                id="syarat-box">
+            <div class="border syarat-grup d-flex justify-content-center align-items-center flex-column col-lg-6 col-md-12" id="syarat-box">
                 <div id="syarat-text">
                     <p>Berjiwa sosial dan mampu bekerja dalam tim.</p>
                     <p>Aktif, kreatif, inovatif, baik dalam pra kegitan, proses kegiatan, maupun pasca
@@ -298,110 +296,110 @@
 </div>
 
 <script>
-$(document).ready(function() {
-    var itemsMainDiv = ('.MultiCarousel');
-    var itemsDiv = ('.MultiCarousel-inner');
-    var itemWidth = "";
+    $(document).ready(function() {
+        var itemsMainDiv = ('.MultiCarousel');
+        var itemsDiv = ('.MultiCarousel-inner');
+        var itemWidth = "";
 
-    $('.leftLst, .rightLst').click(function() {
-        var condition = $(this).hasClass("leftLst");
-        if (condition)
-            click(0, this);
-        else
-            click(1, this)
-    });
-
-    ResCarouselSize();
-
-    $(window).resize(function() {
-        ResCarouselSize();
-    });
-
-    //this function define the size of the items
-    function ResCarouselSize() {
-        var incno = 0;
-        var dataItems = ("data-items");
-        var itemClass = ('.item');
-        var id = 0;
-        var btnParentSb = '';
-        var itemsSplit = '';
-        var sampwidth = $(itemsMainDiv).width();
-        var bodyWidth = $('body').width();
-        $(itemsDiv).each(function() {
-            id = id + 1;
-            var itemNumbers = $(this).find(itemClass).length;
-            btnParentSb = $(this).parent().attr(dataItems);
-            itemsSplit = btnParentSb.split(',');
-            $(this).parent().attr("id", "MultiCarousel" + id);
-
-
-            if (bodyWidth >= 1200) {
-                incno = itemsSplit[2] - 2;
-                itemWidth = sampwidth / incno;
-            } else if (bodyWidth >= 992) {
-                incno = itemsSplit[2] - 3;
-                itemWidth = sampwidth / incno;
-            } else if (bodyWidth >= 768) {
-                incno = itemsSplit[2] - 3;
-                itemWidth = sampwidth / incno;
-            } else if (bodyWidth >= 480) {
-                incno = itemsSplit[2] - 4;
-                itemWidth = sampwidth / incno;
-            } else {
-                incno = itemsSplit[0];
-                itemWidth = sampwidth / incno;
-            }
-            $(this).css({
-                'transform': 'translateX(0px)',
-                'width': itemWidth * itemNumbers
-            });
-            $(this).find(itemClass).each(function() {
-                $(this).outerWidth(itemWidth);
-            });
-
-            $(".leftLst").addClass("over");
-            $(".rightLst").removeClass("over");
-
+        $('.leftLst, .rightLst').click(function() {
+            var condition = $(this).hasClass("leftLst");
+            if (condition)
+                click(0, this);
+            else
+                click(1, this)
         });
-    }
 
-    //this function used to move the items
-    function ResCarousel(e, el, s) {
-        var leftBtn = ('.leftLst');
-        var rightBtn = ('.rightLst');
-        var translateXval = '';
-        var divStyle = $(el + ' ' + itemsDiv).css('transform');
-        var values = divStyle.match(/-?[\d\.]+/g);
-        var xds = Math.abs(values[4]);
-        if (e == 0) {
-            translateXval = parseInt(xds) - parseInt(itemWidth * s);
-            $(el + ' ' + rightBtn).removeClass("over");
+        ResCarouselSize();
 
-            if (translateXval <= itemWidth / 2) {
-                translateXval = 0;
-                $(el + ' ' + leftBtn).addClass("over");
-            }
-        } else if (e == 1) {
-            var itemsCondition = $(el).find(itemsDiv).width() - $(el).width();
-            translateXval = parseInt(xds) + parseInt(itemWidth * s);
-            $(el + ' ' + leftBtn).removeClass("over");
+        $(window).resize(function() {
+            ResCarouselSize();
+        });
 
-            if (translateXval >= itemsCondition - itemWidth / 2) {
-                translateXval = itemsCondition;
-                $(el + ' ' + rightBtn).addClass("over");
-            }
+        //this function define the size of the items
+        function ResCarouselSize() {
+            var incno = 0;
+            var dataItems = ("data-items");
+            var itemClass = ('.item');
+            var id = 0;
+            var btnParentSb = '';
+            var itemsSplit = '';
+            var sampwidth = $(itemsMainDiv).width();
+            var bodyWidth = $('body').width();
+            $(itemsDiv).each(function() {
+                id = id + 1;
+                var itemNumbers = $(this).find(itemClass).length;
+                btnParentSb = $(this).parent().attr(dataItems);
+                itemsSplit = btnParentSb.split(',');
+                $(this).parent().attr("id", "MultiCarousel" + id);
+
+
+                if (bodyWidth >= 1200) {
+                    incno = itemsSplit[2] - 2;
+                    itemWidth = sampwidth / incno;
+                } else if (bodyWidth >= 992) {
+                    incno = itemsSplit[2] - 3;
+                    itemWidth = sampwidth / incno;
+                } else if (bodyWidth >= 768) {
+                    incno = itemsSplit[2] - 3;
+                    itemWidth = sampwidth / incno;
+                } else if (bodyWidth >= 480) {
+                    incno = itemsSplit[2] - 4;
+                    itemWidth = sampwidth / incno;
+                } else {
+                    incno = itemsSplit[0];
+                    itemWidth = sampwidth / incno;
+                }
+                $(this).css({
+                    'transform': 'translateX(0px)',
+                    'width': itemWidth * itemNumbers
+                });
+                $(this).find(itemClass).each(function() {
+                    $(this).outerWidth(itemWidth);
+                });
+
+                $(".leftLst").addClass("over");
+                $(".rightLst").removeClass("over");
+
+            });
         }
-        $(el + ' ' + itemsDiv).css('transform', 'translateX(' + -translateXval +
-            'px)');
-    }
 
-    //It is used to get some elements from btn
-    function click(ell, ee) {
-        var Parent = "#" + $(ee).parent().attr("id");
-        var slide = $(Parent).attr("data-slide");
-        ResCarousel(ell, Parent, slide);
-    }
+        //this function used to move the items
+        function ResCarousel(e, el, s) {
+            var leftBtn = ('.leftLst');
+            var rightBtn = ('.rightLst');
+            var translateXval = '';
+            var divStyle = $(el + ' ' + itemsDiv).css('transform');
+            var values = divStyle.match(/-?[\d\.]+/g);
+            var xds = Math.abs(values[4]);
+            if (e == 0) {
+                translateXval = parseInt(xds) - parseInt(itemWidth * s);
+                $(el + ' ' + rightBtn).removeClass("over");
 
-});
+                if (translateXval <= itemWidth / 2) {
+                    translateXval = 0;
+                    $(el + ' ' + leftBtn).addClass("over");
+                }
+            } else if (e == 1) {
+                var itemsCondition = $(el).find(itemsDiv).width() - $(el).width();
+                translateXval = parseInt(xds) + parseInt(itemWidth * s);
+                $(el + ' ' + leftBtn).removeClass("over");
+
+                if (translateXval >= itemsCondition - itemWidth / 2) {
+                    translateXval = itemsCondition;
+                    $(el + ' ' + rightBtn).addClass("over");
+                }
+            }
+            $(el + ' ' + itemsDiv).css('transform', 'translateX(' + -translateXval +
+                'px)');
+        }
+
+        //It is used to get some elements from btn
+        function click(ell, ee) {
+            var Parent = "#" + $(ee).parent().attr("id");
+            var slide = $(Parent).attr("data-slide");
+            ResCarousel(ell, Parent, slide);
+        }
+
+    });
 </script>
 <?= $this->endSection(); ?>
