4c4532d0c30e630e05c563861fd75ca87fd45516
Ulfatun21 <ulfatun945@gmail.com>
2022-03-24 17:40:07 +0700

    backend to frontend

--- CIbaru/app/Views/frontend/v_artikel.php
+++ CIbaru/app/Views/frontend/v_artikel.php
@@ -14,32 +14,31 @@
             <i class="fas fa-search"></i>
         </div>
     </div>
-    <?php foreach ($blog as $b): ?>
+    <?php foreach ($blog as $b) :
+        $judul = $b['judul'];
+        $cover = $b['cover'];
+        $deskripsi = $b['deskripsi'];
+        $updated_at = $b['updated_at']; ?>
 
-        <!-- </?= $b['judul']; ?> -->
-        
-     <?php endforeach; ?>  
-        
-        
+
+    <?php endforeach; ?>
+
+
 
     <div class="row">
         <div class="posisi-section">
             <div class="col-lg-6 col-md-12">
                 <center>
-                    <img src="frontend2/images/sub-cont.svg" class="card-image">
+                    <img src="/image/<?php echo $cover; ?>" class="card-image">
                 </center>
             </div>
             <div class="col-lg-6 col-md-12 mt-sm-4 mt-lg-0 text-justify">
-                <h2>Bidang Lingkungan</h2>
-                <p class="card-text"><small class="text-muted">Updated 15 Desember 2021</small></p>
+                <h2>
+                    <?php echo $judul; ?>
+                </h2>
+                <p class="card-text"><small class="text-muted"><?php echo $updated_at; ?></small></p>
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                    deserunt mollit anim id est laborum.
+                    <?php echo $deskripsi; ?>
                 </p>
                 <a href="Cdetailartikel" class="btn btn-primary" img src="frontend2/images/READ.svg" style="background: #184D47; font-size:x-small;">Read More</a>
             </div>
@@ -47,22 +46,16 @@
 
         <div class="posisi-section pt-5">
             <div class="col-lg-6 col-md-12 mt-sm-4 mt-lg-0 text-justify">
-                <h2>Bidang Pendidikan</h2>
-                <p class="card-text"><small class="text-muted">Updated 15 Desember 2021</small></p>
+                <h2><?php echo $judul; ?></h2>
+                <p class="card-text"><small class="text-muted"><?php echo $updated_at; ?></small></p>
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                    deserunt mollit anim id est laborum.
+                    <?php echo $deskripsi; ?>
                 </p>
                 <a href="Cdetailartikel" class="btn btn-primary" img src="images/READ.svg" style="background: #184D47; font-size:x-small;">Read More</a>
             </div>
             <div class="col-lg-6 col-md-12">
                 <center>
-                    <img src="frontend2/images/sub-cont.svg" class="card-image">
+                    <img src="/image/<?php echo $cover; ?>" class="card-image">
                 </center>
             </div>
         </div>
@@ -70,20 +63,14 @@
         <div class="posisi-section pt-5">
             <div class="col-lg-6 col-md-12">
                 <center>
-                    <img src="frontend2/images/sub-cont.svg" class="card-image">
+                    <img src="/image/<?php echo $cover; ?>" class="card-image">
                 </center>
             </div>
             <div class="col-lg-6 col-md-12 mt-sm-4 mt-lg-0 text-justify">
-                <h2>Bidang Kesehatan</h2>
-                <p class="card-text"><small class="text-muted">Updated 15 Desember 2021</small></p>
+                <h2><?php echo $judul; ?></h2>
+                <p class="card-text"><small class="text-muted"><?php echo $updated_at; ?></small></p>
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                    deserunt mollit anim id est laborum.
+                    <?php echo $deskripsi; ?>
                 </p>
                 <a href="Cdetailartikel" class="btn btn-primary" img src="frontend2/images/READ.svg" style="background: #184D47; font-size:x-small;">Read More</a>
             </div>
@@ -91,22 +78,16 @@
 
         <div class="posisi-section pt-5">
             <div class="col-lg-6 col-md-12 mt-sm-4 mt-lg-0 text-justify">
-                <h2>Bidang Ekonomi Kreatif</h2>
-                <p class="card-text"><small class="text-muted">Updated 15 Desember 2021</small></p>
+                <h2><?php echo $judul; ?></h2>
+                <p class="card-text"><small class="text-muted"><?php echo $updated_at; ?></small></p>
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                    deserunt mollit anim id est laborum.
+                    <?php echo $deskripsi; ?>
                 </p>
                 <a href="Cdetailartikel" class="btn btn-primary" img src="frontend2/images/READ.svg" style="background: #184D47; font-size:x-small;">Read More</a>
             </div>
             <div class="col-lg-6 col-md-12">
                 <center>
-                    <img src="frontend2/images/sub-cont.svg" class="card-image">
+                    <img src="/image/<?php echo $cover; ?>" class="card-image">
                 </center>
             </div>
         </div>
